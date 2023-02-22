17572a6293c8ecd057e960e1cc15c71d0a77be6e
tahawy111 <amertahawy111@gmail.com>
2023-02-23 01:04:27 +0200

    finish

---
+++ main.js
@@ -0,0 +1,35 @@
+const hr = document.getElementById("hr");
+const mn = document.getElementById("mn");
+const sc = document.getElementById("sc");
+
+const day = new Date();
+
+const hh = day.getHours() * 30;
+const mm = day.getMinutes() * 6;
+const ss = day.getSeconds() * 6;
+
+setInterval(() => {
+  const day = new Date();
+
+  const hh = day.getHours() * 30;
+  const mm = day.getMinutes() * 6;
+  const ss = day.getSeconds() * 6;
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+  // Digital Clock
+
+  const time = new Date();
+  const hours = document.getElementById("hours");
+  const minutes = document.getElementById("minutes");
+  const seconds = document.getElementById("seconds");
+  const ampm = document.getElementById("ampm");
+
+  //   Show Digital Time
+  const h =
+    time.getHours() === 0 ? 12 : time.getHours() > 12 && time.getHours() - 12;
+  hours.innerHTML = h;
+  minutes.innerHTML = time.getMinutes();
+  seconds.innerHTML = time.getSeconds();
+  ampm.innerHTML = time.getHours() > 12 ? "PM" : "AM";
+});
